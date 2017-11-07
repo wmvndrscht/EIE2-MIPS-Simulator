819491ce7989bcff6d2e967bebf0c6050a476c97
wmvndrscht <wmvndrscht@gmail.com>
2017-11-07 12:01:49 +0000

    Added array of mem and reg

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -2,7 +2,27 @@
 #include <iostream>
 
 
+//what to do today
+//Decide simplest instructions j and jr
+
+//Load binary file into memory
+//Initialise registers and Program counter
+//
+
+
+//Anredi to fill in: How to deal with Jump instruction:.............
+
+
+void decode_instr(const int instruction&){
+	
+}
+
 int main(){
+
+	uint8_t MEM[0xFFFFFFFF];
+	uint32_t REG[32];
+
+
  
  std::cout << "HELLO ANDREI" << std::endl;
  std::cout << "HELLO WIM!" << std::endl;   
