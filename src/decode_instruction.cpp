13cfacba2418c11ceb31a7e41882c932829b317d
wmvndrscht <wmvndrscht@gmail.com>
2017-11-15 17:01:11 +0000

    some more structuring

--- src/decode_instruction.cpp
+++ src/decode_instruction.cpp
@@ -22,16 +22,7 @@ string decode_instructionRIJ(const uint32_t& instruction)
 
 void execute_R_type(const instructionR& Rtype){
 
-	// if(type=='R')
-	// {
-	// 	instr_field[1]=(instruction << 6)>>27;  // first source register
-	// 	instr_field[2]=(instruction << 11)>>27; // second source register
-	// 	instr_field[3]=(instruction << 16)>>27; // destination register
-	// 	instr_field[4]=(instruction << 21)>>27; // shift amount
-	// 	instr_field[5]=(instruction << 26)>>26;	// function code
-
-	switch(Rtype->opcode)
-		
+	switch(Rtype.opcode){
 		case 0b100000:
 			return "ADD";
 		case 0b100001:
@@ -52,7 +43,7 @@ void execute_R_type(const instructionR& Rtype){
 			return "MULT";
 		case 0b011001:
 			return "MULTU";
-		case ==0b100101:
+		case 0b100101:
 			return "OR";		// it might be a NOOP
 		case 0b000000:			// it might be a NOOP
 			return "SLL"; 
@@ -76,57 +67,54 @@ void execute_R_type(const instructionR& Rtype){
 			return "SYSCALL";							// WTF is this?
 		case 0b100110:
 			return "XOR";
+		}
 
 }
 
 
-void execute_I_type(const instructionJ& Jtype){
+void execute_I_type(const instructionI& Jtype){
 	
-	if(type=='I')
-	{
-		instr_field[1]=(instruction << 6)>>27;  // first source register
-		instr_field[2]=(instruction << 11)>>27; // second source register
-		instr_field[3]=(instruction << 16)>>16; // immediate
-		
-		if(instr_field[0]==0b001110)
+	switch(Itype.opcode){
+		case 0b001110:
 			return "XORI";
-		if(instr_field[0]==0b101011)
+		case 0b101011:
 			return "SW";
-		if(instr_field[0]==0b001011)
+		case 0b001011:
 			return "SLTIU";
-		if(instr_field[0]==0b001010)
+		case 0b001010:
 			return "SLTI";
-		if(instr_field[0]==0b101000)
+		case 0b101000:
 			return "SB";
-		if(instr_field[0]==0b001101)
+		case 0b001101:
 			return "ORI";
-		if(instr_field[0]==0b100011)
+		case 0b100011:
 			return "LW";	
-		if(instr_field[0]==0b001111)
+		case 0b001111:
 			return "LUI";
-		if(instr_field[0]==0b100000)
+		case 0b100000:
 			return "LB";
-		if(instr_field[0]==0b000101)
+		case 0b000101:
 			return "BNE";
-		if((instr_field[0]==0b000001)&&(instr_field[2]==0b10000))
-			return "BLTZAL";
-		if((instr_field[0]==0b000001)&&(instr_field[2]==0b0))
-			return "BLTZ"; 
-		if(instr_field[0]==0b000111)
+		case 0b000001:
+			if(opcode.rt == 0b10000)
+				return "BLTZAL";
+			if(opcode.rt == 0b0)
+				return "BLTZ"; 
+			if(opcode.rt == 0b10001)
+				return "BGEZAL"
+			if(opcode.rt == 0b00001)
+				return "BGEZ"
+		case 0b000111:
 			return "BGTZ";
-		if(instr_field[0]==0b000110)
+		case 0b000110:
 			return "BLEZ";
-		if((instr_field[0]==0b000001)&&(instr_field[2]==0b10001))
-			return "BGEZAL";
-		if((instr_field[0]==0b000001)&&(instr_field[2]==0b00001))
-			return "BGEZ";
-		if(instr_field[0]==0b000100)
+		case 0b000100: //cool
 			return "BEQ";
-		if(instr_field[0]==0b001100)
+		case 0b001100:
 			return "ANDI";
-		if(instr_field[0]==0b001001)
+		case 0b001001:
 			return "ADDIU";
-		if(instr_field[0]==0b001000)
+		case 0b001000:
 			return "ADDI";
 		
 		return "Invalid instruction";	
@@ -134,92 +122,87 @@ void execute_I_type(const instructionJ& Jtype){
 
 
 void execute_J_type(const instructionJ& Jtype){
-	
-	if(type=='J')
-	{
-		instr_field[1]=(instruction<<6); // extract address
-		
-		if(instr_field[0]==0b000010)
+	switch(Jtype->opcode){
+		case 0b000010:
 			return "J";
-		if(instr_field[0]==0b000011)
+		case 0b000011:
 			return "JAL";
 	}
 
 }
 
-execute_ADD() // check for overflow
-{
+execute_ADD(){ // check for overflow
 	REG[instr_field[3]]=REG[instr_field[1]]+REG[instr_field[2]];
 	PC+=4;
 }
-execute_ADDI() // check for overflow
-{
+execute_ADDI(){ // check for overflow
 	REG[instr_field[2]]=REG[instr_field[1]]+instr_field[3];
 	PC+=4;
 }
-execute_ADDU()
-{
+execute_ADDU(){
 	REG[instr_field[3]]=REG[instr_field[1]]+REG[instr_field[2]];
 	PC+=4;
 }
-execute_ADDIU()
-{
+execute_ADDIU(){
 	REG[instr_field[2]]=REG[instr_field[1]]+instr_field[3];
 	PC+=4;
 }
-execute_AND()
-{
+execute_AND(){
 	REG[instr_field[3]]=REG[instr_field[2]]&REG[instr_field[1]];
 	PC+=4;
 }
-execute_ANDI()
-{
+execute_ANDI(){
 	REG[instr_field[2]]=REG[instr_field[1]]&instr_field[3];
 	PC+=4;
 }
-execute_BEQ() //fuck branches
-{
+execute_BEQ(){ //fuck branches
 	if(REG[instr_field[1]]==REG[instr_field[2]])
 		PC=instr_field[3]<<2;
 	else{
 		PC+=4;	
 	}
 }
-execute_BGEZ()
-{
+execute_BGEZ(){
 	if(REG[instr_field[1]]==instr_field[3])
 		
 }
 
 
-int main()
-{            
+// int main()
+// {            
  
 
- while(PC!=0){
-	string tp = decode_instruction(MEM[PC]);
- if(tp=="J")                                          	                                           
- 	  execute_J();                                                       
- if(tp=="ADD")
-		execute_ADD();
-if(tp=="ADDI")
-	execute_ADDI();
-if(tp=="ADDU")
-	execute_ADDU();
-if(tp=="ADDIU")
-	execute_ADDIU();
+//  while(PC!=0){
+// 	string tp = decode_instruction(MEM[PC]);
+//  if(tp=="J")                                          	                                           
+//  	  execute_J();                                                       
+//  if(tp=="ADD")
+// 		execute_ADD();
+// if(tp=="ADDI")
+// 	execute_ADDI();
+// if(tp=="ADDU")
+// 	execute_ADDU();
+// if(tp=="ADDIU")
+// 	execute_ADDIU();
 	
 
- 		if(decode_instruction(MEM[PC])=='R'){
- 			execute_R_type(MEM[PC]);			
- 		}
- 		if(decode_instruction(MEM[PC])=='I'){
- 			execute_I_type(MEM[PC]);
- 		}
- 	}                                                                               
-  std::cout << "HELLO ANDREI" << std::endl;                                      
-  std::cout << "HELLO WIM!" << std::endl;                                        
-return 10; }      
+//  		if(decode_instruction(MEM[PC])=='R'){
+//  			execute_R_type(MEM[PC]);			
+//  		}
+//  		if(decode_instruction(MEM[PC])=='I'){
+//  			execute_I_type(MEM[PC]);
+//  		}
+//  	}                                                                               
+//   std::cout << "HELLO ANDREI" << std::endl;                                      
+//   std::cout << "HELLO WIM!" << std::endl;                                        
+// return 10; }      
 
   
 
+	// if(type=='R')
+	// {
+	// 	instr_field[1]=(instruction << 6)>>27;  // first source register
+	// 	instr_field[2]=(instruction << 11)>>27; // second source register
+	// 	instr_field[3]=(instruction << 16)>>27; // destination register
+	// 	instr_field[4]=(instruction << 21)>>27; // shift amount
+	// 	instr_field[5]=(instruction << 26)>>26;	// function code
