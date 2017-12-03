e051893b892d0992fb5c7b3838c49691403c011a
wmvndrscht <wmvndrscht@gmail.com>
2017-12-03 22:10:17 +0000

    renaming some test files

--- bin/T2_0_ADDU.s
+++ bin/T2_0_ADDU.s
@@ -1,4 +1,5 @@
 lui $2, 0x8000                                                             
 addu $2, $2, $2                                                             
 jr $0                                                                      
-#expected 0  
+#expected 0 ???
+#expected 1
