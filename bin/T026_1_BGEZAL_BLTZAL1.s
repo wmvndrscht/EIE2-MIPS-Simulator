5d924479c47d86c6843ae28ef1156376d07fc629
Pietreanu <andrei_pietreanu@yahoo.com>
2017-12-06 17:32:05 +0000

    bgezal and bltzal

---
+++ bin/T026_1_BGEZAL_BLTZAL1.s
@@ -0,0 +1,17 @@
+addi $4, $4, -1
+slt $4, 0
+ori $5, $5, 31
+sllv $1, $4, $5
+
+#BLTZAL $1, go2
+BGEZAL $1, go1
+BLTZAL $1, go2
+
+go1:
+ori $2, $0, 16 
+jalr $0
+
+go2:
+xori $2, $0, 14
+jr $0
+#14
