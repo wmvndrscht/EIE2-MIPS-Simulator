57b520aedd3231bcdf20120e4ac1fe3dfb97f19b
Pietreanu <andrei_pietreanu@yahoo.com>
2017-12-08 16:13:12 +0000

    lwr lwl

--- bin/T033_2_LWL.s
+++ bin/T033_2_LWL.s
@@ -1,16 +1,16 @@
-lui $3, 0xfcae
-ori $3, $3, 0x00dd
-lui $4, 0xddfc
-ori $4, $4, 0xae00
+lui $3, 0xcc00
+ori $3, $3, 0xfcae
+lui $4, 0xfcae
+ori $4, $4, 0x00cc
 lui $5, 0x2200
 ori $5, $5, 0x4
 sw $3, 0($5)
 sw $4, 4($5)
 #lhu $6, ($5)
-lwl $6, 5($5)
+lwl $6, 1($5)
 #lhu $7, 6($5)
 #sll $7, $7, 16
-lwr $7, 2($5)
+lwr $7, 6($5)
 beq $6, $7, good
 jr $0
 
--- bin/T033_9_LWL.s
+++ bin/T033_9_LWL.s
@@ -1,25 +1,25 @@
 lui $3, 0x9546
 ori $3, $3, 0x1122
-lui $4, 0x3344
-ori $4, 0x5473
+#lui $4, 0x3344
+#ori $4, 0x5473
 lui $5, 0x2000
-ori $5, $5, 0x8
-#lui $6, 0x7896
-#ori $6, $6, 0xabcd
+ori $5, $5, 0x4358
+lui $6, 0x7896
+ori $6, $6, 0xabcd
 #lui $7, 0xfefe
 #ori $7, $7, 0xdddd
 sw $3, 0($5)
-sw $4, 4($5)
+#sw $4, 4($5)
 lwl $6, 2($5)
-lwr $7, 5($5)
+#lwr $7, 5($5)
 #sll $7, $7, 16
-add $6, $6, $7
+#add $6, $6, $7
 #0411fffsub $6, $6, $3
 lui $8, 0x1122
-ori $8, $8, 0x3344
+ori $8, $8, 0xabcd
 beq $6, $8, good
 jr $0
 
 good:
-xori $2, $2, 16
+addi $2, $2, 20
 jr $0
